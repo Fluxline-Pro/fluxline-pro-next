7f29f3ebdda1cad60a29315bd919f4a5f30eb6e7
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-02-21 14:00:01 -0700

    feat: remove eager loading from image in UnifiedPageWrapper

--- src/components/UnifiedPageWrapper.tsx
+++ src/components/UnifiedPageWrapper.tsx
@@ -653,7 +653,6 @@ export const UnifiedPageWrapper: React.FC<UnifiedPageWrapperProps> = ({
                   objectFit: 'cover',
                   filter: filter,
                 }}
-                loading='eager'
                 priority
                 placeholder='blur'
                 blurDataURL='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABmX/9k='
