3c7b979aa2843c690736cc84156e133cbb7a8659
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-03-16 14:29:04 -0600

    Add overflow hidden style to CompanyTimeline and Hero components

--- src/theme/components/hero/Hero.tsx
+++ src/theme/components/hero/Hero.tsx
@@ -161,6 +161,7 @@ export const Hero: React.FC<HeroProps> = ({
           justifyContent: 'flex-start',
           gap: isMobile ? '0.25rem' : theme.spacing.xs,
           flexWrap: isMobile ? 'nowrap' : 'wrap',
+          overflow: 'hidden',
           paddingLeft: backArrow ? '0.5rem' : undefined, // for the back arrow icon
         }}
       >
